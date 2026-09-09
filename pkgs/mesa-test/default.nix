bb0260531363345481e12ff2b57000ecfab94986
fzdslr <fzdslr@qq.com>
2026-09-09 23:23:40 +0800

    feat: add mesa-test

---
+++ pkgs/mesa-test/default.nix
@@ -0,0 +1,14 @@
+{
+  pkgs,
+}:
+
+if pkgs.stdenv.hostPlatform.isAarch64 then
+  pkgs.mesa.overrideAttrs (oldAttrs: {
+    patches = (oldAttrs.patches or [ ]) ++ [
+      ./41072.patch
+      ./43893.patch
+      ./44053.patch
+    ];
+  })
+else
+  pkgs.mesa
